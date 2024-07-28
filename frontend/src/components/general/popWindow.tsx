7b273071cea14e578518df981213f846def25d51
gumquat <gumquat@gmail.com>
2024-07-28 10:33:45 -0500

    modified dropdown to accept and argument 'array of strings' called customOptions, and added a close button to settings

--- frontend/src/components/general/popWindow.tsx
+++ frontend/src/components/general/popWindow.tsx
@@ -1,27 +1,50 @@
-import React from "react";
-import Button from "./button";
+import React, { useRef, useEffect } from "react";
 import Dropdown from "./dropdown";
+import Button from "./button";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../navigation/navigation.css";
 
 interface PopWindowProps {
   onClose: () => void;
 }
 
 const PopWindow: React.FC<PopWindowProps> = ({ onClose }) => {
+  const windowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        windowRef.current &&
+        !windowRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div className="pop-window">
-      <div className="pop-window-content">
+    <div className="pop-window-overlay">
+      <div className="pop-window" ref={windowRef}>
         <Button
           icon={faTimes}
           onClick={onClose}
           className="close-button"
-          label="Close"
+          label=""
         />
-        <h2>Settings</h2>
-        <Dropdown />
-        <Dropdown />
-        <Dropdown />
-        {/* put more settings options HERE */}
+        <div className="pop-window-content">
+          <div className="pop-window-header">
+            <h2>Settings</h2>
+          </div>
+          <Dropdown />
+          <Dropdown />
+          <Dropdown />
+          {/* put more settings options HERE */}
+        </div>
       </div>
     </div>
   );

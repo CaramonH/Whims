719dbb7c56ad6a3860444662511ec4af53664bac
Evan Newman-Chock <gumquat@gmail.com>
2025-01-07 13:53:40 -0600

    explanatory comments on Login and Account

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,60 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
-import './Login.css';
-import '../card/card'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import "./Login.css";
+import "../card/card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LikeDislike from "../functional/likeDislike";
-import { faUtensils, faMusic, faFilm, faGamepad, faPlaneDeparture, faPalette, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUtensils,
+  faMusic,
+  faFilm,
+  faGamepad,
+  faPlaneDeparture,
+  faPalette,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
+// This is a fake data generator for the landing page cards
 const colorVariables: string[] = [
-  "--color-turq", "--color-mant", "--color-apg", "--color-yell",
-  "--color-org", "--color-red", "--color-ind", "--color-purp",
+  "--color-turq",
+  "--color-mant",
+  "--color-apg",
+  "--color-yell",
+  "--color-org",
+  "--color-red",
+  "--color-ind",
+  "--color-purp",
 ];
-
+// Font Awesome icons for the landing page cards
 const faIcons: any[] = [
-  faUtensils, faMusic, faFilm, faGamepad, faPlaneDeparture, faPalette, faQuestionCircle,
+  faUtensils,
+  faMusic,
+  faFilm,
+  faGamepad,
+  faPlaneDeparture,
+  faPalette,
+  faQuestionCircle,
 ];
-
+// Fake landing page card titles
 const landingTitle: string[] = [
-  "Who is Whims for? - Indecisive Friends!", "What is Whims for? - What games to play together!", "Why Whims? - Simple organization!",
-  "Who is Whims for? - Coworkers!", "What is Whims for? - Deciding on lunch plans!", "Why Whims? - Ease of access!",
-  "Who is Whims for? - Families!", "What is Whims for? - Deciding what to do on Holidays!", "Why Whims? - To build a community!",
-  "Who is Whims for? - Partners and Relationships!", "What is Whims for? - What movies to watch!", "Why Whims? - Ease of communication!",
+  "Who is Whims for? - Indecisive Friends!",
+  "What is Whims for? - What games to play together!",
+  "Why Whims? - Simple organization!",
+  "Who is Whims for? - Coworkers!",
+  "What is Whims for? - Deciding on lunch plans!",
+  "Why Whims? - Ease of access!",
+  "Who is Whims for? - Families!",
+  "What is Whims for? - Deciding what to do on Holidays!",
+  "Why Whims? - To build a community!",
+  "Who is Whims for? - Partners and Relationships!",
+  "What is Whims for? - What movies to watch!",
+  "Why Whims? - Ease of communication!",
 ];
-
+// Helper function to get a random color
 const getRandomColor = (previousColor: string): string => {
   let randomColor: string = getRandomColorHelper();
   while (randomColor === previousColor) {
     randomColor = getRandomColorHelper();
   }
-  console.log('return:', randomColor);
+  console.log("return:", randomColor);
   return randomColor;
 };
-
+// Helper function to get a random Font Awesome icon
 const getRandomFaIcon = (): any => {
   return faIcons[Math.floor(Math.random() * faIcons.length)];
 };
-
+// Helper function to get a random color
 const getRandomColorHelper = (): string => {
   return colorVariables[Math.floor(Math.random() * colorVariables.length)];
 };
-
+// Helper function to get a random fake event name
 const getRandomFakeEventName = (): string => {
   return landingTitle[Math.floor(Math.random() * landingTitle.length)];
 };
-
+// Helper function to generate random card data
 const generateRandomCardData = () => ({
   color: getRandomColor(""),
   faIcon: getRandomFaIcon(),
   landingTitle: getRandomFakeEventName(),
 });
 
+//////////////////////////////////////////
+///////Login component starts here////////
+//////////////////////////////////////////
 const Login: React.FC = () => {
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [isRegistering, setIsRegistering] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [nameError, setNameError] = useState<string | null>(null);
@@ -65,13 +103,15 @@ const Login: React.FC = () => {
   const firestore = getFirestore();
 
   useEffect(() => {
-    const initialCards = Array(3).fill(null).map(() => generateRandomCardData());
+    const initialCards = Array(3)
+      .fill(null)
+      .map(() => generateRandomCardData());
     setCardsData(initialCards);
     console.log("Initial cards set:", initialCards);
   }, []);
 
   const updateCardData = (index: number) => {
-    setCardsData(prevData => {
+    setCardsData((prevData) => {
       const newData = [...prevData];
       newData[index] = generateRandomCardData();
       console.log(`Card ${index} updated:`, newData[index]);
@@ -84,14 +124,19 @@ const Login: React.FC = () => {
     console.log(`Animation ended for card ${index}`);
   };
 
+  ////////////////////////////////////////////////////////
+  ///// login and registration functions start HERE //////
+  ////////////////////////////////////////////////////////
   const handleLogin = async () => {
     setErrorMessage(null);
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (error) {
       console.error("Error logging in", error);
-      setErrorMessage("Failed to log in. Please check your email and password.");
+      setErrorMessage(
+        "Failed to log in. Please check your email and password."
+      );
     }
   };
 
@@ -105,18 +150,22 @@ const Login: React.FC = () => {
     }
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       await setDoc(doc(firestore, "users", user.uid), {
         name: name,
-        email: email
+        email: email,
       });
 
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (error: any) {
       console.error("Error registering", error);
-      if (error.code === 'auth/weak-password') {
+      if (error.code === "auth/weak-password") {
         setErrorMessage("Password must be at least 6 characters long.");
       } else {
         setErrorMessage("Failed to register. Please try again.");
@@ -130,51 +179,60 @@ const Login: React.FC = () => {
     setIsRegistering(!isRegistering);
   };
 
-const handleGoogleLogin = async () => {
-  setErrorMessage(null);
-  const provider = new GoogleAuthProvider();
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
+  // Google login function
+  const handleGoogleLogin = async () => {
+    setErrorMessage(null);
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
-    const name = user.displayName || '';
-    const email = user.email || '';
+      const name = user.displayName || "";
+      const email = user.email || "";
 
-    await setDoc(doc(firestore, "users", user.uid), {
-      name: name,
-      email: email
-    }, { merge: true });  // Add merge: true here
+      await setDoc(
+        doc(firestore, "users", user.uid),
+        {
+          name: name,
+          email: email,
+        },
+        { merge: true }
+      ); // Add merge: true here
 
-    navigate('/dashboard');
-  } catch (error) {
-    console.error("Error logging in with Google", error);
-    setErrorMessage("Failed to log in with Google.");
-  }
-};
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Error logging in with Google", error);
+      setErrorMessage("Failed to log in with Google.");
+    }
+  };
 
-
+  // Return the JSX for the Login component
   return (
     <>
       <div className="landing-whims-div">
         <div className="fake-cards-div">
           {cardsData.map((card, index) => (
-            <div 
-              className="fake-card" 
+            <div
+              className="fake-card"
               key={`card-container-${index}-${JSON.stringify(card)}`}
               onAnimationEnd={() => handleAnimationEnd(index)}
-              onAnimationIteration={() => console.log(`Animation iterated for card ${index}`)}
+              onAnimationIteration={() =>
+                console.log(`Animation iterated for card ${index}`)
+              }
             >
               <div className={`card ${card.color}`}>
                 <div className="card-title">
-                  {card.landingTitle.split('-').map((word: string, i: number) => (
-                    i === 0 ? <h5 key={i}>{word} </h5> : <p key={i}>{word}</p>
-                  ))} 
-                </div>              
+                  {card.landingTitle
+                    .split("-")
+                    .map((word: string, i: number) =>
+                      i === 0 ? <h5 key={i}>{word} </h5> : <p key={i}>{word}</p>
+                    )}
+                </div>
                 <div className="event-type-icon">
                   <FontAwesomeIcon icon={card.faIcon} />
                 </div>
                 <div className="like-dislike-container">
-                  <LikeDislike groupId={''} whimId={''} userID={''} />
+                  <LikeDislike groupId={""} whimId={""} userID={""} />
                 </div>
                 <div className="location-container"></div>
               </div>
@@ -182,13 +240,13 @@ const handleGoogleLogin = async () => {
           ))}
         </div>
       </div>
-      <div className='login-div'>
-        <h1>{isRegistering ? 'Register' : 'Login'}</h1>
+      <div className="login-div">
+        <h1>{isRegistering ? "Register" : "Login"}</h1>
         <div className="login-input-container">
           {isRegistering && (
             <div>
               <input
-                className='login-input'
+                className="login-input"
                 type="text"
                 placeholder="Name"
                 value={name}
@@ -198,14 +256,14 @@ const handleGoogleLogin = async () => {
             </div>
           )}
           <input
-            className='login-input'
+            className="login-input"
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
-            className='login-input'
+            className="login-input"
             type="password"
             placeholder="Password"
             value={password}
@@ -214,15 +272,25 @@ const handleGoogleLogin = async () => {
         </div>
         {errorMessage && <p className="login-error">{errorMessage}</p>}
         <div className="login-button-container">
-          <button className='login-button' onClick={isRegistering ? handleRegister : handleLogin}>
-            {isRegistering ? 'Register' : 'Login'}
+          <button
+            className="login-button"
+            onClick={isRegistering ? handleRegister : handleLogin}
+          >
+            {isRegistering ? "Register" : "Login"}
           </button>
-          <button className='login-button google-login' onClick={handleGoogleLogin}>
+          <button
+            className="login-button google-login"
+            onClick={handleGoogleLogin}
+          >
             Sign in with Google
           </button>
-          <p className="blurb-p">{isRegistering ? 'Don\'t have an account with us? Join here!' : 'Have an account already? Sign in!'}</p>
-          <button className='login-button' onClick={toggleRegistering}>
-            {isRegistering ? 'Login' : 'Create an account'}
+          <p className="blurb-p">
+            {isRegistering
+              ? "Don't have an account with us? Join here!"
+              : "Have an account already? Sign in!"}
+          </p>
+          <button className="login-button" onClick={toggleRegistering}>
+            {isRegistering ? "Login" : "Create an account"}
           </button>
         </div>
       </div>
